abe5f364c073e3e3bdd4150e899c5774f91cfe9d
aleferpar <aleferpar2@alu.edu.gva.es>
2025-02-06 19:12:46 +0100

    2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import {Destinos} from './database.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function botonSiguiente(){
+  
+}
+function botonAnterior(){
+
 }
 
+function App() {
+
+  return (
+    <>
+      <h1>Votacion para el Viaje de final de curso</h1>
+      <div>
+      {Destinos.map((destino, index) => {
+        return (
+          <div key={index}>
+            <h2>{destino.Nombre}</h2>
+            <img src={destino.Imagen} alt={destino.Nombre} />
+            <p>{destino.Descripcion}</p>
+            <p>Precio: {destino.Precio}</p>
+            <button>Me gusta</button>
+          </div>
+        );
+      })}
+
+      </div>
+
+      <div>
+        <button onClick={botonAnterior}>Anterior</button>
+        <button onClick={botonSiguiente}>Siguiente</button>
+      </div>
+      
+    </>
+  );
+}
 export default App;
---
+++ src/database.js
@@ -0,0 +1,7 @@
+export let Destinos = [
+    { Nombre: "Cancun", Precio: 1000, Descripcion: "Un lugar mu bonito", Imagen: "https://www.viajejet.com/wp-content/viajes/una-laguna-en-cancun-1.jpg" },
+    { Nombre: "Miami", Precio: 1500, Descripcion: "Un lugar mu bonito segundo", Imagen: "https://www.viajejet.com/wp-content/viajes/una-laguna-en-cancun-1.jpg" },
+    { Nombre: "Paris", Precio: 2000, Descripcion: "Un lugar mu bonito tercer", Imagen: "https://www.viajejet.com/wp-content/viajes/una-laguna-en-cancun-1.jpg" },
+    { Nombre: "Roma", Precio: 2500, Descripcion: "Un lugar mu bonito cuarto", Imagen: "https://www.viajejet.com/wp-content/viajes/una-laguna-en-cancun-1.jpg" },
+    { Nombre: "Madrid", Precio: 3000, Descripcion: "Un lugar mu bonito quinto", Imagen: "https://www.viajejet.com/wp-content/viajes/una-laguna-en-cancun-1.jpg" },
+]
